cb4cac7085cf3207bc0b8aa79b0e8eea2d64b16c
slalom0914 <slalom0914@gmail.com>
2022-11-30 17:48:36 +0900

    TestParam비교분석

--- app/src/main/java/dev_java/ch01/TestParam.java
+++ app/src/main/java/dev_java/ch01/TestParam.java
@@ -7,6 +7,7 @@ class Param {
 public class TestParam {
   void effectParam(Param p) {
     // insert here - 여기에 p = new Param()이 있으면 결과가 달라진다 아니다 같다?????
+    p = new Param();
     p.ival = 500;
     System.out.println("sub ival ===> " + p.ival);
   }
