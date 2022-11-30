517dec33fe82de6555a7cfc079083f9426a4900f
slalom0914 <slalom0914@gmail.com>
2022-11-30 12:43:32 +0900

    Hello소스수정완료

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -1,8 +1,9 @@
 package dev_java.ch01;
 
 public class Hello {
-  //오늘은 개발 환경 다시 설정
+  // 오늘은 개발 환경 다시 설정
   public static void main(String[] args) {
+    System.out.println("git설정 추가");
     System.out.println("Hello Java!!!");
   }
 }
