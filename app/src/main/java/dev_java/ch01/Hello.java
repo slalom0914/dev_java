88ad0d0898b6da2b16ae8fd454551ba62c6591f0
slalom0914 <slalom0914@gmail.com>
2022-11-30 14:51:11 +0900

    test

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("git설정 추가");
     System.out.println("Hello Java!!!");
+    System.out.println("111");
   }
 }
