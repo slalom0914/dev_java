5368dc87ed91859e446c219734715c630a49f680
키위 <slalom0914@gmail.com>
2025-12-11 13:01:04 +0900

    클래스 나누기 연습완성

--- src/main/java/anet/mvc/basic/ChildJDialog.java
+++ src/main/java/anet/mvc/basic/ChildJDialog.java
@@ -1,20 +1,40 @@
 package anet.mvc.basic;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChildJDialog extends JDialog implements ActionListener {
+    JButton btn_ok = new JButton("확인");
+    JButton btn_cancel = new JButton("취소");
+    JScrollPane jsp = new JScrollPane();
+    JPanel jp_south = new JPanel();
+    ParentJFrame pj = null;
+    public ChildJDialog() {}
     public ChildJDialog(JFrame owner) {
         super(owner, "Child JDialog", false);
+        //아래 낱말카드를 쓸 수 있다.
+        pj = (ParentJFrame) owner;
+        initDisplay();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        Object obj = e.getSource();
+        //확인버튼을 누른거야
+        if(obj == btn_ok){
+            pj.refresh();
+        }
     }
     public void initDisplay() {
+        btn_ok.addActionListener(this);
         this.setTitle("입력|수정|상세보기");
+        jp_south.setLayout(new FlowLayout(FlowLayout.CENTER));
+        jp_south.add(btn_ok);
+        jp_south.add(btn_cancel);
+        this.add("South", jp_south);
+        this.add("Center", jsp);
         this.setSize(300, 300);
         this.setVisible(false);
         //this.dispose();
--- src/main/java/anet/mvc/basic/ParentJFrame.java
+++ src/main/java/anet/mvc/basic/ParentJFrame.java
@@ -8,6 +8,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ParentJFrame extends JFrame implements ActionListener {
+    ChildJDialog childDlg = new ChildJDialog(this);
     JPanel  jp_north = new JPanel();
     JButton btn_insert = new  JButton("입력");
     JButton btn_update = new  JButton("수정");
@@ -15,23 +16,32 @@ public class ParentJFrame extends JFrame implements ActionListener {
     JButton btn_select = new  JButton("조회");
     JButton btn_detail = new  JButton("상세조회");
     JScrollPane jsp_center = new JScrollPane();
+    public void refresh(){
+        System.out.println("refresh 호출 성공");
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         //입력 버튼 클릭 되었을 때
         if (source == btn_insert) {
             System.out.println("입력");
+            childDlg.setTitle("입력");
+            //아래 코드를 추가하는 이유는 최초 setVisible파라미터값이
+            //false이므로
+            childDlg.setVisible(true);
         }//end of 입력
 
         //수정 버튼 클릭 되었을 때
         else if (source == btn_update) {
             System.out.println("수정");
-
+            childDlg.setTitle("수정");
+            childDlg.setVisible(true);
         }//end of 수정
         //상세조회 버튼 클릭 되었을 때
         else if (source == btn_detail) {
             System.out.println("상세조회");
-
+            childDlg.setTitle("상세조회");
+            childDlg.setVisible(true);
         }//end of 상세조회
         //삭제 버튼 클릭 되었을 때
         //조회 버튼 클릭 되었을 때
@@ -54,8 +64,6 @@ public class ParentJFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setVisible(true);
-        ChildJDialog childJDialog = new ChildJDialog(this);
-        childJDialog.initDisplay();
     }//end of initDisplay
 
     public static void main(String[] args) {
